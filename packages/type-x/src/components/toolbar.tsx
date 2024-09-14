547e080cc526beb5dcb6498da76573c7daedc16c
OgabekYuldoshev <yuldashoff1@gmail.com>
2024-09-14 13:13:18 +0500

    feat: ⚡️ create undo redo

--- packages/type-x/src/components/toolbar.tsx
+++ packages/type-x/src/components/toolbar.tsx
@@ -1,5 +1,22 @@
 import { useTypexContext } from "@/hooks/useTypex";
 import { renderBaseMarks } from "@/constants";
+import { createActionButtons } from "./action-buttons";
+import { Separator } from "./ui/separator";
+
+const renderHistoryCommands = createActionButtons(({ editor, t }) => [
+  {
+    title: t("x.undo"),
+    icon: "Undo",
+    onClick: () => editor.chain().undo().run(),
+    disabled: !editor.can().undo() || false,
+  },
+  {
+    title: t("x.redo"),
+    icon: "Redo",
+    onClick: () => editor.chain().redo().run(),
+    disabled: !editor.can().redo() || false,
+  },
+]);
 
 const Toolbar = () => {
   const {
@@ -7,7 +24,9 @@ const Toolbar = () => {
     translation: { t },
   } = useTypexContext();
   return (
-    <div className="x-block x-border-b x-px-4 x-py-2 x-space-x-2">
+    <div className="x-flex x-items-center x-border-b x-px-4 x-py-2 x-space-x-2">
+      {renderHistoryCommands({ editor, t })}
+      <Separator orientation="vertical" />
       {renderBaseMarks({ editor, t })}
     </div>
   );
